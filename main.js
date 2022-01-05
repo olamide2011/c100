3102424db4bbc322095b9ef5b276d2020c5ad168
olamide2011 <91997622+olamide2011@users.noreply.github.com>
2022-01-04 19:49:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+
+var Recognition=new SpeechRecognition();
+
+function start() {
+    Recognition.start();
+    document.getElementById("textbox").innerHTML="";
+
+}
+Recognition.onresult=function(event) {
+    var Content=event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML=Content;
+    if (Content=="take my selfie") {
+        speak();
+    }
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data ="taking selfie in 5 seconds";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+Webcam.attach(camera);
+setTimeout(function(){
+    take_snapshot();
+    save();
+}, 5000);
+}
+camera =document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+png_quality:90
+});
+function take_snapshot() {
+
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML ='<img id="selfie_image" src="'+data_uri+'"/>';
+
+    });
+}
+function save() {
+    link= document.getElementById("link");
+    image= document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click()    
+}
